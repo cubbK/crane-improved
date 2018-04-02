4b963ecf32bf82b23f11d7df053b93f44751ed14
Dan <danpopa1998@gmail.com>
2018-04-02 17:10:15 +0300

    add function to check if can add all containers

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 const data = require('./data.json')
-console.log(data)
+const R = require('ramda')
+
+const canLift = R.curry(
+  (craneMaxWeight, containerWeight) => craneMaxWeight >= containerWeight
+)
+
+/**
+ * Returns true if can load all containers with the provided crane 
+ * 
+ * @param {object} crane 
+ * @param {number[]} containers
+ * @returns {boolean} 
+ */
+function canLoadAllContainers (crane, containers) {
+  const canCraneLift = canLift(crane.maxWeight)
+
+  return R.all(canCraneLift)(containers)
+}
+
+const crane = data.cranes[0]
+const containersToLoad = data.containers.splice(0, 5)
+
+const can = canLoadAllContainers(crane, containersToLoad)
+
+
+// Conditia 1 
+// De incarcat 5 containere si daca macar unul e prea greu de anulat tot
+
+
+
+console.log(can)
